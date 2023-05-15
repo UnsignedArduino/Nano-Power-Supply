fc207c855169741e741dc12bd311e80d47f9352a
UnsignedArduino <UnsignedArduino@outlook.com>
2023-05-15 19:29:48 -0400

    Display digit at position

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,10 @@ const uint8_t SHIFT_CS = 9;
 const uint8_t SHIFT_CLK = 10;
 const uint8_t SHIFT_DATA = 8;
 
-const uint8_t digitTable[] = {
+const uint8_t SEGMENT_NUM = 4;
+const uint8_t SEGMENT_CONTROL[SEGMENT_NUM] = {7, 6, 5, 4};
+
+const uint8_t DIGIT_TABLE[] = {
   0b00111111, // 0
   0b00000110, // 1
   0b01011011, // 2
@@ -17,24 +20,36 @@ const uint8_t digitTable[] = {
   0b01101111  // 9
 };
 
-void setup() {
-  Serial.begin(9600);
-
+void beginDigits() {
   pinMode(SHIFT_CS, OUTPUT);
   pinMode(SHIFT_CLK, OUTPUT);
   pinMode(SHIFT_DATA, OUTPUT);
+  for (uint8_t i = 0; i < SEGMENT_NUM; i++) {
+    pinMode(SEGMENT_CONTROL[i], OUTPUT);
+    digitalWrite(SEGMENT_CONTROL[i], LOW);
+  }
 }
 
-void displayDigit(uint8_t num, bool dp = false) {
+void displayDigit(uint8_t num, uint8_t pos, bool dp = false, uint16_t d = 5) {
   digitalWrite(SHIFT_CS, LOW);
   shiftOut(SHIFT_DATA, SHIFT_CLK, MSBFIRST,
-           digitTable[num] | (dp ? 0b10000000 : 0));
+           DIGIT_TABLE[num] | (dp ? 0b10000000 : 0));
   digitalWrite(SHIFT_CS, HIGH);
+  digitalWrite(SEGMENT_CONTROL[pos], HIGH);
+  delay(d);
+  digitalWrite(SEGMENT_CONTROL[pos], LOW);
+}
+
+void setup() {
+  Serial.begin(9600);
+
+  beginDigits();
 }
 
 void loop() {
-  for (uint8_t i = 0; i < 10; i++) {
-    displayDigit(i);
-    delay(500);
+  for (uint8_t i = 0; i < 4; i++) {
+    for (uint8_t j = 0; j < 10; j++) {
+      displayDigit(j, i, false, 500);
+    }
   }
 }

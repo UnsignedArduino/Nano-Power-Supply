a5c1880735261e735186c166580a340d2e8ea59c
UnsignedArduino <UnsignedArduino@outlook.com>
2023-05-15 23:19:42 -0400

    Polish example

--- src/main.cpp
+++ src/main.cpp
@@ -1,68 +1,41 @@
 #include <Arduino.h>
-/* SevSegShift Counter Example
+#include <SevSegShift.h>
 
- Copyright 2020 Dean Reading,
- Copyright 2020 Jens Breidenstein
+const uint8_t SHIFT_PIN_SHCP = 6;
+const uint8_t SHIFT_PIN_STCP = 7;
+const uint8_t SHIFT_PIN_DS = 8;
 
- This example demonstrates a very simple use of the SevSegShift library with a 4
- digit display. It displays a counter that counts up, showing deci-seconds.
- */
+const uint8_t numDigits = 4;
+const uint8_t digitPins[] = {5, 4, 3, 2};
+const uint8_t segmentPins[] = {0, 2, 4, 6, 7, 1, 3, 5};
+const bool resistorsOnSegments = true;
+const uint8_t hardwareConfig = COMMON_CATHODE;
+const bool updateWithDelays = false;
+const bool leadingZeros = false;
+const bool disableDecPoint = false;
 
-#include "SevSegShift.h"
-
-#define SHIFT_PIN_SHCP 6
-#define SHIFT_PIN_STCP 7
-#define SHIFT_PIN_DS 8
-
-/* Instantiate a seven segment controller object with:
-  - segment pins controlled via 1 shift register and
-  - digit pins connected to the Arduino directly
-  */
-SevSegShift
-  sevsegshift(SHIFT_PIN_DS, SHIFT_PIN_SHCP, SHIFT_PIN_STCP,
-              1,   /* number of shift registers there is only 1 Shiftregister
-                      used for all Segments (digits are on Controller)
-                      default value = 2 (see SevSegShift example)
-                      */
-              true /* Digits are connected to Arduino directly
-                      default value = false (see SevSegShift example)
-                    */
-  );
+SevSegShift display(SHIFT_PIN_DS, SHIFT_PIN_SHCP, SHIFT_PIN_STCP, 1, true);
 
 void setup() {
-  byte numDigits = 4;
-  byte digitPins[] = {5, 4, 3, 2}; // These are the PINS of the ** Arduino **
-  byte segmentPins[] = {
-    0, 2, 4, 6, 7, 1, 3, 5}; // these are the PINs of the ** Shift register **
-  bool resistorsOnSegments = true; // 'false' means resistors are on digit pins
-  byte hardwareConfig = COMMON_CATHODE; // See README.md for options
-  bool updateWithDelays = false;      // Default 'false' is Recommended
-  bool leadingZeros =
-    false; // Use 'true' if you'd like to keep the leading zeros
-  bool disableDecPoint =
-    false; // Use 'true' if your decimal point doesn't exist or isn't connected
-
-  sevsegshift.begin(hardwareConfig, numDigits, digitPins, segmentPins,
-                    resistorsOnSegments, updateWithDelays, leadingZeros,
-                    disableDecPoint);
-  sevsegshift.setBrightness(90);
+  display.begin(hardwareConfig, numDigits, (uint8_t*)digitPins,
+                (uint8_t*)segmentPins, resistorsOnSegments, updateWithDelays,
+                leadingZeros, disableDecPoint);
+  display.setBrightness(90);
 }
 
 void loop() {
-  static unsigned long timer = millis();
-  static int deciSeconds = 0;
+  static uint32_t timer = millis();
+  static uint16_t decaS = 0;
 
-  if (millis() - timer >= 100) {
-    timer += 100;
-    deciSeconds++; // 100 milliSeconds is equal to 1 deciSecond
+  if (millis() - timer >= 10) {
+    timer += 10;
+    decaS++;
 
-    if (deciSeconds == 10000) { // Reset to 0 after counting for 1000 seconds.
-      deciSeconds = 0;
+    if (decaS == 10000) {
+      decaS = 0;
     }
-    sevsegshift.setNumber(deciSeconds, 1);
+    display.setNumber(decaS, 2);
   }
 
-  sevsegshift.refreshDisplay(); // Must run repeatedly
+  display.refreshDisplay();
 }
-
-/// END ///
